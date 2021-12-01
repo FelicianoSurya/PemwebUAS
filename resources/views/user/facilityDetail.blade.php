b15bc4b58c5470d0cb00f6645eac67226cd01c5c
Fkhorasani <fernando.khorasani@gmail.com>
2021-12-01 10:21:26 +0700

    facility detail

--- resources/views/user/facilityDetail.blade.php
+++ resources/views/user/facilityDetail.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container pt-5">
         <div class="ataslist row justify-content-between py-5">
             <div class="judul col-lg-6 col-md-6 col-12 row ">
                 <span style="color: #372074;">Facilities</span>
@@ -16,35 +16,20 @@
             </div>
         </div>
     </div>
-    <div class="isidetailF row col-lg-6 col-12 px-3 justify-content-evenly">
-        <div class="pictF text-center mx-auto">
-            <img src="https://picsum.photos/300/200" alt="" class="rounded p-5">
-        </div>
-        <div class="textDetail col-lg-6 col-12 ps-2 py-md-2 py-sm-1 mx-auto">
-            <h1>Basketball Court</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consectetur, rem eum ullam expedita aperiam ipsam temporibus veritatis quae unde ab! Deleniti corporis laudantium illum maxime, provident illo ad suscipit in harum libero veritatis odit, accusantium eligendi labore accusamus voluptatum mollitia ipsum, explicabo quisquam ab. Soluta perspiciatis ea, debitis doloribus minima commodi nesciunt tenetur impedit corrupti, deleniti repellendus fugit ratione.</p>
-            <a href="#" class="btn btn-light col-3">Detail</a>
-            <a href="#" class="btn btn-light col-3 mx-2">Favorite &hearts;</a>
+    <div class="backgroundDesc">
+        <div class="isidetailF row px-3 justify-content-evenly mx-auto">
+            <div class="pictF col-lg-6 col-md-6 col-12 text-center">
+                <img src="https://picsum.photos/500/400" alt="" class="rounded p-5">
+            </div>
+            <div class="textDetail col-lg-4 col-md-6 col-12 py-lg-5 ps-md-2 py-md-2 py-sm-1">
+                <h1>Basketball Court</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consectetur, rem eum ullam expedita aperiam ipsam temporibus veritatis quae unde ab! Deleniti corporis laudantium illum maxime, provident illo ad suscipit in harum libero veritatis odit, accusantium eligendi labore accusamus voluptatum mollitia ipsum, explicabo quisquam ab. Soluta perspiciatis ea, debitis doloribus minima commodi nesciunt tenetur impedit corrupti, deleniti repellendus fugit ratione.</p>
+                <a href="#" class="btn btn-light">Detail</a>
+                <a href="#" class="btn btn-light mx-2">Favorite &hearts;</a>
+            </div>
         </div>
     </div>
-    <div class="container justify-content-start pt-3">
-        <h1>Description</h1>
-        <div class="isiDesc row">
-                    <p class="col-2"> 
-                       Court aspect <br>
-                       Length<br>
-                       Width<br>
-                       Height<br>
-                       Playing area
-                    </p>
-                    <p class="col-2">
-                      Dimension<br>
-                      100 m<br>
-                      200 m<br>
-                      10 m<br>
-                      500 m <sup>2</sup>
-                    </p>
-                  </div>
+    
 
     </div>
 @endsection
