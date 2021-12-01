7c0e9c43281ef303de52b75c951838439b234e66
Fkhorasani <fernando.khorasani@gmail.com>
2021-12-01 11:16:27 +0700

    login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
+@section('custom-css')
+<link rel="stylesheet" href="{{asset('css/login.css')}}">
+@endsection
+
 @section('content')
-<div class="container">
+<div class="container loginContainer d-flex align-items-center justify-content-center">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-12">
+            <div class="card p-3">
+                <div class="card-header text-center">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="username" class="col-md-4 col-form-label">{{ __('Username') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required  autofocus>
 
                                 @error('username')
@@ -25,10 +29,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,10 +44,16 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-light px-4">
                                     {{ __('Login') }}
                                 </button>
+                            </div>
+                            <div class="col-md-12 d-flex text-center flex-column pt-3">
+                                <span>Don't have an acccount?</span>
+                                <span>
+                                   <a href="" style="color: #FFB13E;">Register</a>
+                                </span>
                             </div>
                         </div>
                     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
+@section('custom-css')
+<link rel="stylesheet" href="{{asset('css/login.css')}}">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="container loginContainer d-flex align-items-center justify-content-center">
+    <div class="row justify-content-center w-50">
+        <div class="col-md-12">
+            <div class="card p-3">
+                <div class="card-header text-center">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="name" class="col-md-4 col-form-label">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -25,10 +29,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="username" class="col-md-4 col-form-label">{{ __('Username') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="email" value="{{ old('username') }}" required>
 
                                 @error('username')
@@ -39,10 +43,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -53,17 +57,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="form-group row flex-column">
+                            <label for="password-confirm" class="col-md-6 col-form-label">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-light px-4">
                                     {{ __('Register') }}
                                 </button>
                             </div>
