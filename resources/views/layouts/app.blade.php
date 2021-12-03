034ed38577c7ba06f9fa52ea87160ca1f09a9509
FelicianoSurya <suryamarcello@gmail.com>
2021-12-03 15:46:59 +0700

    logo title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <title>Booking Court</title>
+    <link rel = "icon" href ="{{ asset('images/logo.png') }}" type = "image/x-icon">
     
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
